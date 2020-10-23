5f6e8418b5df43334aab7337d3a88cadb37f8487
François Juteau <fjuteau@vente-privee.com>
2020-10-23 15:43:30 +0200

    Try workaround

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
         .iOS(.v9),
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DataDomeSDK",
-            targets: ["DataDomeSDK"]),
+            type: .dynamic,
+            targets: ["DataDomeSDKTarget", "DataDomeSDK"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "DataDomeSDKTarget"),
         .binaryTarget(
             name: "DataDomeSDK",
             url: "https://package.datadome.co/ios/DataDomeSDK-2.5.5.zip",
